147b45d397bfc91b2efb5df74ab05381f2cbdb47
TheSwirlingVoid <rnsugavanam@gmail.com>
2023-03-14 15:25:18 -0700

    ok i fixed it for real this time

--- lua/language/lsp.lua
+++ lua/language/lsp.lua
@@ -3,10 +3,14 @@ require("language.settings.nativelsp")
 local keybindings = require("keybindings.bindings")
 
 
-vim.g.coq_settings = { auto_start = 'shut-up' }
-vim.cmd([[
-	let g:coq_settings = { "keymap.recommended.cr": v:false }
-]])
+-- do you know how long it took me to figure out
+-- how to remove COQ's stupid carriage return binding?
+vim.g.coq_settings = {
+	auto_start = 'shut-up',
+	keymap = {
+		recommended = false
+	}
+}
 
 local servers = {
 	"pyright",

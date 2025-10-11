0e451f2ab45977ac9c9e5fe239eda93ee486109f
Raj Sugavanam <rnsugavanam@gmail.com>
2025-10-11 13:57:49 -0500

    Forgot to comment out a file

--- lua/presence/presenceconfig.lua
+++ lua/presence/presenceconfig.lua
@@ -20,96 +20,96 @@
 --     line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
 --     terminal_text       = "Using Terminal",           -- Format string rendered when in terminal mode.})
 -- })
-require('cord').setup({
-    enabled = true,
-    log_level = vim.log.levels.OFF,
-    editor = {
-        client = 'neovim',
-        tooltip = 'Neovim',
-        icon = nil,
-    },
-    display = {
-        -- theme = 'onyx',
-        swap_fields = false,
-        swap_icons = false,
-    },
-    timestamp = {
-        enabled = true,
-        reset_on_idle = false,
-        reset_on_change = false,
-    },
-    idle = {
-        enabled = true,
-        timeout = 300000,
-        show_status = true,
-        ignore_focus = true,
-        unidle_on_focus = false,
-        smart_idle = true,
-        details = 'Idling...',
-        state = nil,
-        tooltip = '💤',
-        icon = nil,
-    },
-    text = {
-        workspace = function(opts) return 'In workspace ' .. opts.workspace end,
-        viewing = function(opts) return 'Viewing ' .. opts.filename end,
-        editing = function(opts) return 'Writing ' .. opts.filename end,
-        file_browser = function(opts) return 'Browsing files in ' .. opts.name end,
-        plugin_manager = function(opts) return 'Managing plugins in ' .. opts.name end,
-        lsp = function(opts) return 'Configuring LSP in ' .. opts.name end,
-        docs = function(opts) return 'Reading ' .. opts.name end,
-        vcs = function(opts) return 'Committing changes in ' .. opts.name end,
-        notes = function(opts) return 'Taking notes in ' .. opts.name end,
-        debug = function(opts) return 'Debugging in ' .. opts.name end,
-        test = function(opts) return 'Testing in ' .. opts.name end,
-        diagnostics = function(opts) return 'Fixing problems in ' .. opts.name end,
-        games = function(opts) return 'Playing ' .. opts.name end,
-        terminal = function(opts) return 'Running commands in ' .. opts.name end,
-        dashboard = 'Home',
-    },
-    -- buttons = nil,
-    buttons = {
-        {
-            label = 'View Repository',
-            url = function(opts) return opts.repo_url end,
-        },
-    },
-    assets = {
-        ['.sv'] = {
-            icon = require('cord.api.icon').get('v'),
-            tooltip = "SystemVerilog",
-            type="language"
-        }
-    },
-    variables = nil,
-    hooks = {
-        ready = nil,
-        shutdown = nil,
-        pre_activity = nil,
-        post_activity = nil,
-        idle_enter = nil,
-        idle_leave = nil,
-        workspace_change = nil,
-    },
-    plugins = nil,
-    advanced = {
-        plugin = {
-            autocmds = true,
-            cursor_update = 'on_hold',
-            match_in_mappings = true,
-        },
-        server = {
-            update = 'fetch',
-            pipe_path = nil,
-            executable_path = nil,
-            timeout = 300000,
-        },
-        discord = {
-            reconnect = {
-                enabled = true,
-                interval = 5000,
-                initial = true,
-            },
-        },
-    },
-})
+-- require('cord').setup({
+--     enabled = true,
+--     log_level = vim.log.levels.OFF,
+--     editor = {
+--         client = 'neovim',
+--         tooltip = 'Neovim',
+--         icon = nil,
+--     },
+--     display = {
+--         -- theme = 'onyx',
+--         swap_fields = false,
+--         swap_icons = false,
+--     },
+--     timestamp = {
+--         enabled = true,
+--         reset_on_idle = false,
+--         reset_on_change = false,
+--     },
+--     idle = {
+--         enabled = true,
+--         timeout = 300000,
+--         show_status = true,
+--         ignore_focus = true,
+--         unidle_on_focus = false,
+--         smart_idle = true,
+--         details = 'Idling...',
+--         state = nil,
+--         tooltip = '💤',
+--         icon = nil,
+--     },
+--     text = {
+--         workspace = function(opts) return 'In workspace ' .. opts.workspace end,
+--         viewing = function(opts) return 'Viewing ' .. opts.filename end,
+--         editing = function(opts) return 'Writing ' .. opts.filename end,
+--         file_browser = function(opts) return 'Browsing files in ' .. opts.name end,
+--         plugin_manager = function(opts) return 'Managing plugins in ' .. opts.name end,
+--         lsp = function(opts) return 'Configuring LSP in ' .. opts.name end,
+--         docs = function(opts) return 'Reading ' .. opts.name end,
+--         vcs = function(opts) return 'Committing changes in ' .. opts.name end,
+--         notes = function(opts) return 'Taking notes in ' .. opts.name end,
+--         debug = function(opts) return 'Debugging in ' .. opts.name end,
+--         test = function(opts) return 'Testing in ' .. opts.name end,
+--         diagnostics = function(opts) return 'Fixing problems in ' .. opts.name end,
+--         games = function(opts) return 'Playing ' .. opts.name end,
+--         terminal = function(opts) return 'Running commands in ' .. opts.name end,
+--         dashboard = 'Home',
+--     },
+--     -- buttons = nil,
+--     buttons = {
+--         {
+--             label = 'View Repository',
+--             url = function(opts) return opts.repo_url end,
+--         },
+--     },
+--     assets = {
+--         ['.sv'] = {
+--             icon = require('cord.api.icon').get('v'),
+--             tooltip = "SystemVerilog",
+--             type="language"
+--         }
+--     },
+--     variables = nil,
+--     hooks = {
+--         ready = nil,
+--         shutdown = nil,
+--         pre_activity = nil,
+--         post_activity = nil,
+--         idle_enter = nil,
+--         idle_leave = nil,
+--         workspace_change = nil,
+--     },
+--     plugins = nil,
+--     advanced = {
+--         plugin = {
+--             autocmds = true,
+--             cursor_update = 'on_hold',
+--             match_in_mappings = true,
+--         },
+--         server = {
+--             update = 'fetch',
+--             pipe_path = nil,
+--             executable_path = nil,
+--             timeout = 300000,
+--         },
+--         discord = {
+--             reconnect = {
+--                 enabled = true,
+--                 interval = 5000,
+--                 initial = true,
+--             },
+--         },
+--     },
+-- })
